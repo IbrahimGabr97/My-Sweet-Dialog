c8742a0fb99ecf03daa831bdf6ede7c51dc1a3b7
IbrahimGabr97 <ibrahimgabr97@gmail.com>
2022-01-05 21:43:24 +0200

    blink library added

---
+++ app/src/main/java/com/hope/igb/sweetdialog/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hope.igb.sweetdialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
